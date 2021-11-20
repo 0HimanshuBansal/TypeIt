68a7571d16cfedd367d0622e01abd2212e24ac33
0HimanshuBansal <himanshubansal791999@gmail.com>
2021-11-21 03:52:34 +0530

    initial commit

---
+++ client/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container my-3">
+            <div className="card text-white bg-dark mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">TypeIt</h5>
+                    <p className="card-text">We are an company that provides varous utility mobile-application and web-application for free, mostly.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ server/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// const mongoURI = "mongodb://localhost:27017/TypeIt?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = "mongodb+srv://HimanshuAtlas:KVguJCcuxPF2Mcn@cluster0.eehl6.mongodb.net/TypeIt?retryWrites=true&w=majority";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connect To Mongo");
+    })
+}
+
+module.exports = connectToMongo;
